0c99ece527e5cb0849f760f82a6cf95c9ef8b6d4
jinyoung02 <jink8962@naver.com>
2025-04-27 21:41:16 +0900

    init

---
+++ src/main/java/com/meltin/meltinbackend/MeltinApplication.java
@@ -0,0 +1,13 @@
+package com.meltin.meltinbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeltinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeltinApplication.class, args);
+
+    }
+}
---
+++ src/test/java/com/meltin/meltinbackend/MeltinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meltin.meltinbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeltinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
